9105f786b1ee7672329d821a6e9cfd32b811fe0b
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-25 21:44:04 +0000

    update with Leetcode #46 question, example and constraint

---
+++ index.js
@@ -0,0 +1,13 @@
+// Leetcode #46 permutations
+//Constraints: 
+// 1 <= nums.length <= 6
+// -10 <= nums[i] <= 10
+// All the integers of nums are unique.
+
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ nums = [1,2,3]
+ var permute = function(nums) {
+
+ }
